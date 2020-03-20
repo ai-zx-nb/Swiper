7b732c146f087dda76d95c55e16d740ecbe78fdc
Seamile <lanhuermao@gmail.com>
2020-03-20 16:55:19 +0800

    通过 MonkeyPatch 方式为 ORM 增加了缓存处理

--- common/keys.py
+++ common/keys.py
@@ -4,3 +4,4 @@ VCODE_K = 'Vcode-%s'  # 短信验证码的 Key
 FIRST_RCMD_Q = 'FIRST_RCMD_Q-%s'  # 优先推荐队列的 key，拼接 uid
 REWIND_K = 'Rewind-%s'  # 反悔数据的 Key
 USER_RPROFILE_K = 'UserProfile-%s'  # 用户个人资料的 key
+MODEL_K = 'Model-%s-%s'  # Model 模型的 Key
--- swiper/__init__.py
+++ swiper/__init__.py
@@ -1,4 +1,8 @@
 import pymysql
 
+from libs.orm import patch_model
+
 pymysql.version_info = (1, 3, 13, "final", 0)
 pymysql.install_as_MySQLdb()
+
+patch_model()
