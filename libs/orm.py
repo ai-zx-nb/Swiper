43dba602ff087ae1611b23ab2655b0282004f962
Seamile <lanhuermao@gmail.com>
2020-03-23 10:41:53 +0800

    添加了统一的 to_dict() 方法

--- libs/orm.py
+++ libs/orm.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.db.models import query
 
@@ -49,6 +51,26 @@ def get(self, *args, **kwargs):
     return model_obj
 
 
+def to_dict(self, exclude=()):
+    '''将当前模型的属性转成 dict 类型'''
+    attr_dict = {}
+
+    # 需要强转成字符串的类型
+    force_str_types = (datetime.datetime, datetime.date, datetime.time)
+
+    for field in self._meta.fields:
+        name = field.attname
+        value = getattr(self, field.attname)
+
+        if name not in exclude:
+            # 将特殊类型强转成 str 类型
+            if isinstance(value, force_str_types):
+                value = str(value)
+            attr_dict[name] = value
+
+    return attr_dict
+
+
 def patch_model():
     '''通过 Monkey Patch 的方式为 DjangoORM 增加缓存处理'''
     # 修改 Model 的 save 方法
@@ -58,3 +80,6 @@ def patch_model():
     # 修改 get 方法
     query.QuerySet._get = query.QuerySet.get
     query.QuerySet.get = get
+
+    # 统一为所有 Model 增加 to_dict 方法
+    models.Model.to_dict = to_dict
--- user/models.py
+++ user/models.py
@@ -64,17 +64,6 @@ class User(models.Model):
         now = datetime.datetime.now()
         return now >= self.vip_end
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'phonenum': self.phonenum,
-            'nickname': self.nickname,
-            'gender': self.gender,
-            'birthday': str(self.birthday),
-            'avatar': self.avatar,
-            'location': self.location,
-        }
-
 
 class Profile(models.Model):
     '''交友资料'''
@@ -91,17 +80,3 @@ class Profile(models.Model):
     vibration = models.BooleanField(default=True, verbose_name='开启震动')
     only_matched = models.BooleanField(default=True, verbose_name='不让陌生人看我的相册')
     auto_play = models.BooleanField(default=True, verbose_name='自动播放视频')
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'dating_gender': self.dating_gender,
-            'dating_location': self.dating_location,
-            'min_distance': self.min_distance,
-            'max_distance': self.max_distance,
-            'min_dating_age': self.min_dating_age,
-            'max_dating_age': self.max_dating_age,
-            'vibration': self.vibration,
-            'only_matched': self.only_matched,
-            'auto_play': self.auto_play,
-        }
